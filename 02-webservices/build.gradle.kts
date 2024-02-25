24aa8977b556b86cd012f1c6071c17034edcedcc
Alexander Erben <alexander.erben@rtl-extern.de>
2024-02-25 09:45:55 +0100

    feat: centralize configuration

--- 02-webservices/build.gradle.kts
+++ 02-webservices/build.gradle.kts
@@ -1,33 +1,6 @@
 plugins {
     id("java")
-    id("org.springframework.boot") version "3.2.2"
-    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "digital.erben.gfu"
 version = "0.0.1-SNAPSHOT"
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-}
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("net.datafaker:datafaker:2.0.2")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.testcontainers:junit-jupiter:1.19.6")
-    testImplementation("org.mock-server:mockserver-client-java:5.15.0")
-    testImplementation("org.testcontainers:mockserver:1.19.6")
-    testImplementation("org.testcontainers:testcontainers:1.19.6")
-    implementation("net.datafaker:datafaker:2.1.0")
-}
-
-tasks {
-    test {
-        useJUnitPlatform()
-    }
-}
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,19 @@ group = "digital.erben.gfu"
 version = "1.0-SNAPSHOT"
 plugins {
     id("com.diffplug.spotless") version "6.25.0" apply false
+    id("org.springframework.boot") version "3.2.2" apply false
+    id("io.spring.dependency-management") version "1.1.4" apply false
+
 }
 repositories {
     mavenCentral()
 }
 
 subprojects {
-    apply(plugin = "com.diffplug.spotless")
+    plugins.apply("com.diffplug.spotless")
+    plugins.apply("java")
+    plugins.apply("org.springframework.boot")
+    plugins.apply("io.spring.dependency-management")
     plugins.withId("com.diffplug.spotless") {
         // Configure Spotless plugin here
         configure<com.diffplug.gradle.spotless.SpotlessExtension> {
@@ -22,6 +28,23 @@ subprojects {
         }
     }
 
+    dependencies {
+
+        "implementation"("org.springframework.boot:spring-boot-starter-webflux")
+        "testImplementation"("org.springframework.boot:spring-boot-starter-test")
+        "testImplementation"("io.projectreactor:reactor-test")
+        "testImplementation"("org.testcontainers:junit-jupiter:1.19.6")
+        "testImplementation"("org.mock-server:mockserver-client-java:5.15.0")
+        "testImplementation"("org.testcontainers:mockserver:1.19.6")
+        "testImplementation"("org.testcontainers:testcontainers:1.19.6")
+        "implementation"("net.datafaker:datafaker:2.1.0")
+    }
+
+    plugins.withType<JavaPlugin>().configureEach {
+        val extension = the<JavaPluginExtension>()
+        extension.sourceCompatibility = JavaVersion.VERSION_17
+        extension.targetCompatibility = JavaVersion.VERSION_17
+    }
 
     // Example of adding common repositories
     repositories {
