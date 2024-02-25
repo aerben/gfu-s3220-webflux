da46ec35ea660b16e287ffc20056c41461212ee8
Alexander Erben <alexander.erben@rtl-extern.de>
2024-02-25 20:11:43 +0100

    feat: more oauth2 examples

--- 04-security-oauth2/src/main/java/digital/erben/reactiveweb/ReactiveWebApplication.java
+++ 04-security-oauth2/src/main/java/digital/erben/reactiveweb/ReactiveWebApplication.java
@@ -1,24 +1,31 @@
 package digital.erben.reactiveweb;
 
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.Map;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
 @SpringBootApplication
 @RestController
 public class ReactiveWebApplication {
+
+    private final WebClient webClient;
+
+    public ReactiveWebApplication(WebClient webClient) {
+        this.webClient = webClient;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ReactiveWebApplication.class, args);
@@ -28,4 +35,18 @@ public class ReactiveWebApplication {
     public Mono<Map<String, Object>> user(@AuthenticationPrincipal OAuth2User principal) {
         return Mono.just(Map.of("name", principal.getAttributes().getOrDefault("name", "NO NAME")));
     }
+
+    @PostMapping("/user/logProfile")
+    public Mono<Void> trackUser(@RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient authorizedClient) {
+        return
+            webClient.get()
+                .uri(URI.create("https://api.github.com/user"))
+                .attributes(oauth2AuthorizedClient(authorizedClient))
+                .retrieve()
+                .toBodilessEntity()
+                .doOnSuccess(System.out::println)
+                .doOnError(System.err::println)
+                .then();
+    }
+
 }
--- 04-security-oauth2/src/main/java/digital/erben/reactiveweb/SecurityConfig.java
+++ 04-security-oauth2/src/main/java/digital/erben/reactiveweb/SecurityConfig.java
@@ -5,10 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CorsSpec;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,16 +24,28 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
         return http
+            .csrf(CsrfSpec::disable)
+            .cors(CorsSpec::disable)
             .authorizeExchange(a -> a
-                .pathMatchers("/", "/error", "/webjars/**")
+                .pathMatchers("/", "/error", "/webjars/**", "/logout")
                 .permitAll()
-                .pathMatchers("/user")
+                .anyExchange()
                 .authenticated()
             )
             .exceptionHandling(e -> e
                 .authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))
             )
+            .logout(l -> l.logoutUrl("/logout"))
             .oauth2Login(withDefaults())
+            .build();
+    }
+
+    @Bean
+    public WebClient oauth2WebClient(ServerOAuth2AuthorizedClientRepository clientRepository,
+                                     ReactiveClientRegistrationRepository reactiveClientRegistrationRepository) {
+        return WebClient.builder()
+            .filter(new ServerOAuth2AuthorizedClientExchangeFilterFunction(reactiveClientRegistrationRepository,
+                clientRepository))
             .build();
     }
 
